312d13711d11e05091b9edd0ce625ba072c0bf90
Mohan K <mohank12092003@gmail.com>
2025-02-13 22:42:03 +0530

    trail

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
-service=Service(executable_path="")
+service=Service(executable_path="/home/mohan/Desktop/whatsapp_automation/chromedriver")
 driver=webdriver.Chrome(service=service)
 
+driver.get("https://google.com")
+
+time.sleep(10)
+driver.quit()
